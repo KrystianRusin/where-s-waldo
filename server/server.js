7adcf166acd6e3b63d6532afe6afecba76243644
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-02-21 16:07:40 -0500

    updated server to listen to ip addresses on 0.0.0.0

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const dotenv = require("dotenv");
 require("dotenv").config();
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
@@ -85,4 +87,6 @@ app.post("/leaderboard", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server started at port 5000"));
+app.listen(port, "0.0.0.0", () =>
+  console.log(`Server started at port ${port}`)
+);
